c270002ba9b5f51953a7a856b5c0648e20cc3beb
ahans03 <ahans03@jaguar.tamu.edu>
2022-03-29 13:18:32 -0500

    Initial Commit

---
+++ Verse Matching/Verse_MatchingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Verse_MatchingApp.swift
+//  Verse Matching
+//
+//  Created by Anthony Hanson on 3/29/22.
+//
+
+import SwiftUI
+
+@main
+struct Verse_MatchingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
